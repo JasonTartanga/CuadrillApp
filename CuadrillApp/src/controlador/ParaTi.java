424c007ba3dcb9dbefe07562e5d65dacb44b141e
Jason <Jason@192.168.1.131>
2023-10-02 21:21:51 +0200

    Primer Commit

---
+++ CuadrillApp/src/controlador/ParaTi.java
@@ -0,0 +1,18 @@
+package controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+
+/**
+ *
+ * @author Jason.
+ */
+public class ParaTi {
+
+    @FXML
+    private Button btnInicio, btnUsuario, btn, btnCrear, btnPerfil;
+
+    @FXML
+    private ToggleButton btnLikes;
+}
